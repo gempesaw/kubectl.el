6783b0cabe2074c5f856627519a5e82926f8dc12
Daniel Gempesaw <gempesaw@gmail.com>
2022-11-20 21:22:24 -0600

    lol

--- kubectl-get-fancy.sh
+++ kubectl-get-fancy.sh
@@ -12,10 +12,9 @@ kubectl_get_pods_output=$(mktemp)
 kubectl_get_before_resources_output=$(mktemp)
 kubectl_get_after_resources_output=$(mktemp)
 
-kube-capacity --namespace $namespace --util --pods -ojson | jq -r '.nodes[].pods[]' | jq -s . > $kube_capacity_output &
+kube-capacity --namespace $namespace --util --pods -ojson | jq -r '.nodes[].pods[]?' | jq -s 'map(if .cpu.limits == "0m" then . else .cpu.utilizationPercent = (((.cpu.utilization | rtrimstr("m") | tonumber) / (.cpu.limits | rtrimstr("m") | tonumber)) * 100 | floor | tostring) + "%" end) | map(if .memory.limits == "0Mi" then . else .memory.utilizationPercent = (((.memory.utilization | rtrimstr("Mi") | tonumber) / (.memory.limits | rtrimstr("Mi") | tonumber)) * 100 | floor | tostring) + "%" end)' > $kube_capacity_output &
 
-kubectl $kube_opts get pods --no-headers -owide | jq --raw-input 'split("\n") | map(splits(" +")) | { "name": .[0], "ready": .[1], "status": .[2], "restarts": .[3], "age": .[4], "ip": .[5], "node": .[6], "nominated node": .[7], "readiness gates": .[8] }' | jq -s . > $kubectl_get_pods_output &
-
+kubectl $kube_opts get pods --no-headers -owide | jq --raw-input 'split("\n") | map(splits("  +")) | { "name": .[0], "ready": .[1], "status": .[2], "restarts": .[3], "age": .[4], "ip": .[5], "node": .[6], "nominated node": .[7], "readiness gates": .[8] }' | jq -s . > $kubectl_get_pods_output &
 
 prefix=""
 if [[ "$resources_before_pods" != "" ]]; then
@@ -35,7 +34,7 @@ if [[ "$resources_before_pods" != "" ]]; then
     echo
 fi
 
-jq -s '[ .[0] + .[1] | group_by(.name)[] | select(length > 1) | add ]' $kube_capacity_output $kubectl_get_pods_output | jq -r '["NAME", "CUse", "CPer", "CReq", "CLim", "MUse", "MPer", "MReq", "MLim", "READY", "STATUS", "RESTARTS", "AGE", "IP", "NODE"], (.[] | ["'"$prefix"'" + .name, .cpu.utilization, .cpu.utilizationPercent, .cpu.requests, .cpu.limits, .memory.utilization, .memory.utilizationPercent, .memory.requests, .memory.limits, .ready, .status, .restarts, .age, .ip, .node ]) | @tsv' | column -t
+jq -s '[ .[0] + .[1] | group_by(.name)[] | add ]' $kubectl_get_pods_output $kube_capacity_output | jq -r '["NAME", "CUse", "CPer", "CReq", "CLim", "MUse", "MPer", "MReq", "MLim", "READY", "STATUS", "RESTARTS", "AGE", "IP", "NODE"], (.[] | ["'"$prefix"'" + .name, .cpu.utilization, .cpu.utilizationPercent, .cpu.requests, .cpu.limits, .memory.utilization, .memory.utilizationPercent, .memory.requests, .memory.limits, .ready, .status, .restarts, .age, .ip, .node ]) | @tsv' | column -ts $'\t'
 
 if [[ "$resources_after_pods" != "" ]]; then
     echo
